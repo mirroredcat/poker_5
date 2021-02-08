9b6b17e781ead0da3743ca7338df33e032c9d0a4
TheClawMonster <negrilaclaudia@gmail.com>
2021-02-08 14:40:49 +0100

    sorting hand from high to low

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,3 +1,53 @@
 class Hand
+  
+  def initialize(hand)
+    @current_hand = hand
+  end
+
+  def sort_cards
+    cards = []
+    if @current_hand.length.odd?
+      t = @current_hand.split("")
+      t.each_with_index{|el, i|
+        if el == "1"
+          t[i] += t[i+1]
+          t.delete_at(i+1)
+          break
+        end
+      }
+      t.each_slice(2){|el| cards << el}
+    else
+      @current_hand.split("").each_slice(2){|el| cards << el}
+    end
+    cards = cards.map{|val,sym| val.to_i == 0 ?  [translate(val), sym] : [val.to_i, sym] }
+    cards.sort.reverse.map{|val,sym| val > 10 ? [translate_nr(val), sym] : [val, sym] }.join
+  end
+
+
+  def translate(lttr)
+    case lttr
+    when "J"
+      11
+    when "Q"
+      12
+    when "K"
+      13
+    when "A"
+      14
+    end
+  end
+
+  def translate_nr(nr)
+    case nr
+    when 11
+      "J"
+    when 12
+      "Q"
+    when 13
+      "K"
+    when 14
+      "A"
+    end
+  end
 
 end
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require 'card'
 require 'deck'
+require 'hand'
 
 describe Card do
   subject(:card) {Card.new(2, "\u2664")}
@@ -15,12 +16,6 @@ describe Card do
     end
   end 
 
-  describe '#use_card' do
-    it "changed the card from unused to used" do
-      card.use_card
-      expect(card.used).to eq(true)
-    end
-  end
 
   describe '#render' do
    it "prints the card value and symbol" do
@@ -35,7 +30,7 @@ end
 
 describe Deck do
   subject(:deck) {Deck.new}
-  let(:card) {double("card")}
+  let(:card1) {double("card", :value => 2, :sym =>"♤" )}
 
   describe '#initialize' do
 
@@ -61,14 +56,21 @@ describe Deck do
 
   describe '#deal' do
     let!(:initial_deck) {deck.cards_left}
+    let(:card2) {double("card", :value => 3, :sym =>"♤" )}
+    let(:card3) {double("card", :value => 4, :sym =>"♤" )}
+    let(:card4) {double("card", :value => 5, :sym =>"♤" )}
+    let(:card5) {double("card", :value => 6, :sym =>"♤" )}
+
     it "removes 5 cards from the deck" do
       deck.deal
       expect(deck.cards_left).to_not eq(initial_deck)
       expect(initial_deck - deck.cards_left).to eq(5)
     end
 
-    it 'returns the 5 cards'
-
+    it 'returns the 5 cards' do
+      allow(deck).to receive(:deal).and_return([card1,card2,card3,card4,card5])
+      expect(deck.deal).to eq([card1,card2,card3,card4,card5])
+    end
 
   end
 
@@ -85,4 +87,88 @@ describe Deck do
     end
   end
 
+  describe '#draw' do
+
+    it 'returns a single card' do
+      allow(deck).to receive(:draw).and_return(card1)
+      expect(deck.draw).to eq(card1)
+    end
+
+
+    it 'decreases the deck by 1' do
+      deck.deal
+      deck.deal
+      deck.draw
+      deck.draw
+      expect(deck.cards_left).to eq(40)
+    end
+  end
+
+  describe '#discard' do
+
+    it 'discards one card without returning it' do
+      deck.deal
+      deck.deal
+      deck.draw
+      deck.draw
+      deck.discard
+      expect(deck.cards_left).to eq(39)
+    end
+  end
+end
+
+
+describe Hand do
+  subject(:hand) {Hand.new("2♤3♤4♤5♤")}
+
+  describe '#score' do
+    it 'returns appropriate score for hand'
+
+  end
+
+  describe '#translate' do
+    it 'turns J-Q-K-A into integers' do
+      expect(hand.translate("A")).to eq(14)
+      expect(hand.translate("J")).to eq(11)
+      expect(hand.translate("Q")).to eq(12)
+      expect(hand.translate("K")).to eq(13)
+    end
+
+  end
+
+  describe '#sort_cards' do
+  let(:hand2) {Hand.new('10♤K♡J♧Q♢')}
+    it 'orderes cards by value' do
+      
+      expect(hand2.sort_cards).to eq('K♡Q♢J♧10♤')
+    end
+  end
+
+  describe '#pair' do
+
+    it 'returns true if hand contains a pair'
+
+    it 'returns false if hand contains no pairs'
+  end
+
+  describe '#three' do
+    it 'returns true if hand contains 3 of the same'
+
+    it 'returns false if hand does not have 3 of the same cards'
+
+  end
+
+  describe '#same_symbol' do
+    it 'returns true if hand has same symbol'
+
+    it 'returns false if hand has different symbols'
+
+  end
+
+  describe '#consecutive' do
+    it 'returns true if all cards are consecutive'
+
+    it 'returns false if cards are not consecutive'
+
+  end
 end
